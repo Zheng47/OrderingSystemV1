4a8a681d88a1f16c3acc152284929827f0982111
AJ Sen <sen.alexanderjonard.deslate@gmail.com>
2025-04-27 21:01:45 +0800

    Add project files.

---
+++ OrderingSystemV1/Components/Pages/Login_SignUp/UserLogin.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace OrderingSystemV1.Components.Pages.Login_SignUp
+{
+    public partial class UserLogin
+    {
+        private string showSignup = "hidden";
+        private string signUpInterface = "";
+        private bool isSignupInterfaceOpen = false;
+
+        private void ShowSignup()
+        {
+            if(!isSignupInterfaceOpen)
+            {
+                signUpInterface = "signup-content";
+                showSignup = string.Empty;
+            } else
+            {
+                showSignup = "hidden";
+            }
+            isSignupInterfaceOpen = !isSignupInterfaceOpen;
+        }
+
+        private async Task CloseSignUp()
+        {
+            if (isSignupInterfaceOpen)
+            {
+                signUpInterface = "signup-content-close";
+                await Task.Delay(1000);
+                showSignup = "hidden";
+            } else
+            {
+                signUpInterface = "signup-content";
+                showSignup = string.Empty;
+            }
+            isSignupInterfaceOpen = !isSignupInterfaceOpen;
+        }
+    }
+}
